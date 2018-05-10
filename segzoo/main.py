e7d92b9106a9c4f3ec5c617ed31edb547e98861a
Marc Asenjo <markasenjo@hotmail.com>
2018-05-10 12:56:21 -0400

    Help made more readable

--- segzoo/main.py
+++ segzoo/main.py
@@ -11,8 +11,15 @@ def main(args=sys.argv[1:]):
     here = path.abspath(path.dirname(__file__))
     default_prefix = path.dirname(path.dirname(path.dirname(path.dirname(here))))
 
-    parser = argparse.ArgumentParser(description='Download necessary files, run workflow and obtain results',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    description = '''
+    Segzoo is a tool that allows to run various genomic analysis on a segmentation obtained by segway.
+    The results of each analysis are made available as well as a summarizing visualization of the results.
+    The tool will download all necessary data into a common directory and run all the analysis, storing
+    the results in an output directory. All this information is then transformed into a set of tables
+    that can be found in this same directory under the "data" folder, that are used to generate a final visualization.
+    '''
+
+    parser = argparse.ArgumentParser(description= description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('segmentation', help='.bed.gz file, the segmentation/annotation output from Segway')
     parser.add_argument('--parameters', default=False,
@@ -21,7 +28,7 @@ def main(args=sys.argv[1:]):
     parser.add_argument('--species', default='Homo_sapiens', help='Species of the genome used for the segmentation')
     parser.add_argument('--build', default='hg38', help='Build of the genome assembly used for the segmentation')
     parser.add_argument('--prefix', default=default_prefix,
-                        help='Prefix where all the GGD data is going to be downloaded')
+                        help='Prefix where all the GGD data is going to be downloaded, followed by /share/ggd/SPECIES/BUILD')
 
     parsed_args = parser.parse_args(args)
     snakemake.snakemake(path.join(here, "Snakefile"), config=vars(parsed_args))  # dryrun=True, printshellcmds=True, printreason=True)
