4a7cb5f57cba13173f1551a97352e11f75e80d97
Marc Asenjo <markasenjo@hotmail.com>
2018-05-14 16:19:44 -0400

    made cores choosable

--- segzoo/main.py
+++ segzoo/main.py
@@ -19,16 +19,19 @@ def main(args=sys.argv[1:]):
     that can be found in this same directory under the "data" folder, that are used to generate a final visualization.
     '''
 
-    parser = argparse.ArgumentParser(description= description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('segmentation', help='.bed.gz file, the segmentation/annotation output from Segway')
     parser.add_argument('--parameters', default=False,
                         help='The params.params file used to obtain the gmtk-parameters')
     parser.add_argument('-o', '--outdir', default='outdir', help='Output directory to store all the results')
+    parser.add_argument('-j', default=1, metavar='CORES', type=int, help='Number of cores to use')
     parser.add_argument('--species', default='Homo_sapiens', help='Species of the genome used for the segmentation')
     parser.add_argument('--build', default='hg38', help='Build of the genome assembly used for the segmentation')
     parser.add_argument('--prefix', default=default_prefix,
                         help='Prefix where all the GGD data is going to be downloaded, followed by /share/ggd/SPECIES/BUILD')
 
     parsed_args = parser.parse_args(args)
-    snakemake.snakemake(path.join(here, "Snakefile"), config=vars(parsed_args))  # dryrun=True, printshellcmds=True, printreason=True)
+
+    snakemake.snakemake(path.join(here, "Snakefile"), cores=parsed_args.j, config=vars(parsed_args))
+    # dryrun=True, printshellcmds=True, printreason=True)
