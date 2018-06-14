db5867afe06c36dfae807e6331f7a3eab076819b
Marc Asenjo <markasenjo@hotmail.com>
2018-06-14 14:28:48 -0400

    modularized visualization

--- segzoo/visualization.py
+++ segzoo/visualization.py
@@ -1,12 +1,42 @@
-import pandas as pd
+# import pandas as pd
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from collections import defaultdict
 from os import path
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.transforms import ScaledTranslation
 
 from segzoo.gene_biotypes import BIOTYPES
+
+# VARIABLES AND CONSTANTS
+
+# Table proportion modifiers
+NUM_COMPONENTS = 8
+GMTK_FACTOR = 1
+MIX_FACTOR = 1.5
+AGG_FACTOR = 1
+ROW_CORRECTOR = 1
+
+# Font scaling variables
+FONT_SCALE = 2
+sns.set(font_scale=FONT_SCALE)
+# sns.set_context("poster")
+LABEL_FONTSIZE = 20 * FONT_SCALE / 1.5
+TITLE_FONTSIZE = 25 * FONT_SCALE / 1.5
+
+# Table options and properties
+TABLE_POS = "bottom"  # top / bottom / other to ommit
+TABLE_HEIGHT = 1  # relative to the height of 2 rows from the mix matrix
+TABLE_CONTENT = [['max', 'max', 'max', 'max', 'max', 0.65],
+                 ['min', 'min', 'min', 'min', 'min', 0.35]]
+
+# Color maps for the visualization
+cmap_gmtk = sns.diverging_palette(220, 10, as_cmap=True)
+cmap_mix = 'YlGn'
+cmap_agg = 'Blues'
+
 
 # Prepare the gmtk parameters in a DataFrame
 def gmtk_parameters():
@@ -85,20 +115,6 @@ def aggregation():
     return df_dict, max_value
 
 
-NUM_COMPONENTS = 8
-GMTK_FACTOR = 1
-MIX_FACTOR = 1.5
-AGG_FACTOR = 1
-ROW_CORRECTOR = 1
-
-LABEL_FONTSIZE = 20
-TITLE_FONTSIZE = 25
-
-sns.set(font_scale=1.5)
-
-# Color maps for the visualization
-cmap_gmtk = sns.diverging_palette(220, 10, as_cmap=True)
-
 # Call the functions that obtain the results in DataFrames
 if snakemake.config['parameters']:
     res_gmtk = gmtk_parameters()
@@ -107,6 +123,7 @@ else:
 res_mix_hm, res_mix_ann = mix_data_matrix()
 res_agg_dict, agg_vmax = aggregation()
 
+# Dimensioning variables
 GMTK_COL = res_gmtk.shape[1] * GMTK_FACTOR + 1
 MIX_COL = res_mix_hm.shape[1] * MIX_FACTOR + 1
 AGG_COL = len(BIOTYPES) * NUM_COMPONENTS * AGG_FACTOR + 1
@@ -127,14 +144,18 @@ if snakemake.config['parameters']:
     cbar_gmtk.ax.set_yticklabels([0, 1], fontsize=LABEL_FONTSIZE)
 
     # Setting titles and axis labels
-    ax_gmtk.set_yticklabels(ax_gmtk.get_yticklabels(), rotation=0, fontsize=LABEL_FONTSIZE)  # put label names horizontally
+    ax_gmtk.set_yticklabels(ax_gmtk.get_yticklabels(), rotation=0,
+                            fontsize=LABEL_FONTSIZE)  # put label names horizontally
     ax_gmtk.set_xticklabels(ax_gmtk.get_xticklabels(), rotation=90, fontsize=LABEL_FONTSIZE)
-    ax_gmtk.set_title('GMTK parameters', fontsize=TITLE_FONTSIZE, position=(0, 1 + 0.6 / res_gmtk.shape[0]), ha='left', va='bottom')
+    ax_gmtk.set_title('GMTK parameters',
+                      fontsize=TITLE_FONTSIZE,
+                      position=(0, 1 + 0.6 / res_gmtk.shape[0] * FONT_SCALE / 1.5),
+                      ha='left', va='bottom')
 else:
     f.delaxes(ax_gmtk)
 
 # Mix matrix
-g_mix = sns.heatmap(res_mix_hm, annot=res_mix_ann, cbar=True, cmap='YlGn', vmin=0, vmax=1, ax=ax_mix, fmt='.5g')
+g_mix = sns.heatmap(res_mix_hm, annot=res_mix_ann, cbar=True, cmap=cmap_mix, vmin=0, vmax=1, ax=ax_mix, fmt='.5g')
 cbar_mix = g_mix.collections[0].colorbar
 cbar_mix.set_ticks([0, 1])
 cbar_mix.ax.set_yticklabels(['low', 'high'], fontsize=LABEL_FONTSIZE)
@@ -146,6 +167,37 @@ if snakemake.config['parameters']:
 else:
     ax_mix.set_yticklabels(ax_mix.get_yticklabels(), rotation=0, fontsize=LABEL_FONTSIZE)
 
+# Add min-max table
+mix_columns = res_mix_hm.shape[1]
+
+if TABLE_POS == "bottom":
+    high_low_table = ax_mix.table(
+        cellText=TABLE_CONTENT,
+        cellColours=[[cbar_mix.cmap(0.99)] * mix_columns, [cbar_mix.cmap(0.01)] * mix_columns],
+        bbox=[0, - (TABLE_HEIGHT + .25) / n_rows, 1, TABLE_HEIGHT / n_rows],  # [left,bottom,width,height]
+        fontsize=LABEL_FONTSIZE,
+        cellLoc='center')
+    for j in range(mix_columns):
+        high_low_table._cells[(0, j)]._text.set_color('white')
+
+    # Offset labels down to leave space for the table
+    dx = 0
+    dy = -(TABLE_HEIGHT + 0.25) * 55 / 72
+    offset = ScaledTranslation(dx, dy, f.dpi_scale_trans)
+
+    for label in ax_mix.xaxis.get_majorticklabels():
+        label.set_transform(label.get_transform() + offset)
+
+elif TABLE_POS == "top":
+    high_low_table = ax_mix.table(
+        cellText=TABLE_CONTENT,
+        cellColours=[[cbar_mix.cmap(0.99)] * mix_columns, [cbar_mix.cmap(0.01)] * mix_columns],
+        bbox=[0, 1.02, 1, TABLE_HEIGHT / n_rows],  # [left,bottom,width,height]
+        fontsize=LABEL_FONTSIZE,
+        cellLoc='center')
+    for j in range(mix_columns):
+        high_low_table._cells[(0, j)]._text.set_color('white')
+
 # Aggregation
 stats_df = pd.read_table(snakemake.input.stats, index_col=0)  # data stored when creating the gtf files
 divider = make_axes_locatable(ax_agg)
@@ -154,7 +206,7 @@ title_args = dict(fontsize=LABEL_FONTSIZE, position=(1.0, 1.0), ha='right', va='
 # Divide axes, plot heatmap and edit axis configuration for each biotype
 for biotype in BIOTYPES[1:]:
     ax_agg_aux = divider.append_axes("right", size="100%", pad=0.3)
-    sns.heatmap(res_agg_dict[biotype], annot=True, cbar=False, vmin=0, vmax=agg_vmax, cmap='Blues', ax=ax_agg_aux,
+    sns.heatmap(res_agg_dict[biotype], annot=True, cbar=False, vmin=0, vmax=agg_vmax, cmap=cmap_agg, ax=ax_agg_aux,
                 fmt='.5g')
     ax_agg_aux.set_title('{} (n={})'.format(biotype, stats_df.loc[biotype, 'genes']), **title_args)
     ax_agg_aux.set_yticklabels([])
@@ -163,9 +215,10 @@ for biotype in BIOTYPES[1:]:
 if len(BIOTYPES) > 0:
     ax_agg_cbar = divider.append_axes("right", size=0.35, pad=0.3)
 
-    ax_agg.text(0, -0.6, "Aggregation", fontsize=TITLE_FONTSIZE, ha='left', va='bottom')
-    g_agg = sns.heatmap(res_agg_dict[BIOTYPES[0]], annot=True, cbar=True, vmin=0, vmax=agg_vmax, cbar_ax=ax_agg_cbar,
-                        cmap='Blues', ax=ax_agg, fmt='.5g')
+    ax_agg.text(0, -0.6 * FONT_SCALE / 1.5, "Aggregation", fontsize=TITLE_FONTSIZE, ha='left', va='bottom')
+    g_agg = sns.heatmap(res_agg_dict[BIOTYPES[0]], annot=True, cbar=True, vmin=0, vmax=agg_vmax,
+                        cbar_ax=ax_agg_cbar,
+                        cmap=cmap_agg, ax=ax_agg, fmt='.5g')
     ax_agg.set_title('{} (n={})'.format(BIOTYPES[0], stats_df.loc[BIOTYPES[0], 'genes']), **title_args)
     ax_agg.set_yticklabels([])
     ax_agg.set_xticklabels(ax_agg.get_xticklabels(), rotation=90, fontsize=LABEL_FONTSIZE)
@@ -173,7 +226,8 @@ if len(BIOTYPES) > 0:
     # Edit the colorbar created by the first biotype
     cbar_agg = g_agg.collections[0].colorbar
     cbar_agg.set_ticks([0, agg_vmax])
-    cbar_agg.ax.set_yticklabels(['0%', '{:.0f}%'.format(agg_vmax)], fontsize=LABEL_FONTSIZE)  # the format takes out decimals
+    cbar_agg.ax.set_yticklabels(['0%', '{:.0f}%'.format(agg_vmax)],
+                                fontsize=LABEL_FONTSIZE)  # the format takes out decimals
 else:
     f.delaxes(ax_agg)
 
