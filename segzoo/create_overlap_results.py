cb304891432a88bf23392948f3d4f81e293d1fd1
Ida Liu <ida.liu@mail.utoronto.ca>
2019-06-24 15:25:55 -0400

    Added the script to create overlap results (create_overlap_results.py)

---
+++ segzoo/create_overlap_results.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv(snakemake.input.infile, sep='\t', header=1, index_col=0)
+genic_percentage = df['gene']/df['total']
+intergenic_percentage = df['none']/df['total']
+
+percentage_df = pd.concat([genic_percentage, intergenic_percentage], axis=1, keys=['genic', 'intergenic'])
+percentage_df.to_csv(snakemake.output.outfile, sep='\t')
