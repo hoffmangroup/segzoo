9148cf4f00596f18e9a451f9e9099f7b2654553e
Ida Liu <ida.liu@mail.utoronto.ca>
2019-07-16 12:02:55 -0400

    Changed species to a positional argument and fixed the help message

--- segzoo/main.py
+++ segzoo/main.py
@@ -15,34 +15,34 @@ def main(args=sys.argv[1:]):
     The results of each analysis are made available as well as a summarizing visualization of the results.
     The tool will download all necessary data into a common directory and run all the analysis, storing
     the results in an output directory. All this information is then transformed into a set of tables
-    that can be found in this same directory under the "data" folder, that are used to generate a final visualization.
+    that can be found in this same directory under the "data" folder, that are used to generate a final visualization
+    under the "plots" folder.
     '''
 
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('segmentation', help='.bed.gz file, the segmentation/annotation output from Segway')
+    parser.add_argument('build', default='hg38', help='Build of the genome assembly used for the segmentation')
     parser.add_argument('--parameters', default=False, metavar='params.params',
                         help='The params.params file used to obtain the gmtk-parameters')
     parser.add_argument('-o', '--outdir', default='outdir', help='Output directory to store all the results')
     parser.add_argument('-j', default=1, metavar='CORES', type=int, help='Number of cores to use')
     parser.add_argument('--species', default='Homo_sapiens', help='Species of the genome used for the segmentation')
-    parser.add_argument('--build', default='hg38', help='Build of the genome assembly used for the segmentation')
     parser.add_argument('--prefix', default=default_prefix,
                         help='Prefix where all the external data is going to be downloaded, followed by '
                              '/share/ggd/SPECIES/BUILD')
-    parser.add_argument('--download-only', action='store_true',
-                        help='Execute only the rules that need internet connection, '
-                             'which store data in a shared directory')
-    parser.add_argument('--mne', metavar='MNE_FILE',
-                        help='Allows specify an mne file to translate segment labels and track names on the shown on '
-                             'the figure')
     parser.add_argument('--normalize-gmtk', action='store_true',
                         help='If set, normalize gmtk parameters column wise. Default to True')
     parser.add_argument('--dendrogram', action='store_true',
                         help='If set, perform hierarchical clustering of GMTK parameters table row-wise')
+    parser.add_argument('--mne', metavar='MNE_FILE',
+                        help='Allows specify an mne file to translate segment labels and track names on the shown on '
+                             'the figure')
+    parser.add_argument('--download-only', action='store_true',
+                        help='Execute only the rules that need internet connection, '
+                             'which store data in a shared directory')
     parser.add_argument('--unlock', action='store_true', help='unlock directory (see snakemake doc)')
-
     parsed_args = parser.parse_args(args)
 
     if parsed_args.download_only:
