6eaa5f74e7a18b6a1fab27816d4d5c911dd8e2f0
mmendez12 <mendez.mickael@gmail.com>
2023-02-01 23:44:10 -0500

    update version

--- setup.py
+++ setup.py
@@ -21,14 +21,13 @@ setup(
     author_email='mickael.mendez@mail.utoronto.ca',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        # 'Intended Audience :: Developers',
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         'Programming Language :: Python :: 3.8'
     ],
     packages=find_packages('.'),
     install_requires=['seaborn', 'segtools', 'snakemake', 'pybedtools'],
-    python_requires='==3.8.*',
+    python_requires='>=3.7',
     package_data={  # Optional
         'segzoo': ['Snakefile'],
     },
